88a54494cc7609cd187b7e7aaaefa2e324615e35
Nate O'Meara <omeara.nate@gmail.com>
2021-11-15 23:04:10 -0600

    Merged to one file so global varaibles can work

--- A3.cpp
+++
@@ -1,1 +0,0 @@
-#include "A3.h"
--- main.cpp
+++ main.cpp
@@ -3,15 +3,49 @@
 // Due 11/21/2021
 
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <unistd.h>
+#include "A3.h"
 
 using namespace std;
+
+// Function Prototypes
+void memoryManager(int, int);
+int allocate( int, int);
+
+
+
+
+// Global Variables
+void* freeFrameList;
 
 int main()
 {
 
     cout << "Hello World\n";
+
+    memoryManager(30, 1);
+
+
+
+    return 0;
+}
+
+void memoryManager(int memSize, int frameSize)
+{
+    int freeFrameList[memSize] = {0};
+
+    for (int i =0; i < memSize; i++)
+    {
+        cout << freeFrameList[i] << endl;
+    }
+    
+}
+
+
+int allocate(int allocSize, int pid)
+{
 
 
 
