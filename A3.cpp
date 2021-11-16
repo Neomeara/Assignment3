9664b9a4442e2b35991e9cbf35d9234688a859c7
Nate O'Meara <omeara.nate@gmail.com>
2021-11-15 22:25:39 -0600

    First commit

---
+++ A3.cpp
@@ -0,0 +1,1 @@
+#include "A3.h"
---
+++ main.cpp
@@ -0,0 +1,19 @@
+// CSC 456 Assignment 3
+// Nate O'Meara
+// Due 11/21/2021
+
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+
+using namespace std;
+
+int main()
+{
+
+    cout << "Hello World\n";
+
+
+
+    return 0;
+}
