a6b03146d0202e4a38a98d4610d7cc058cb210df
Nate O'Meara <omeara.nate@gmail.com>
2021-11-17 00:03:55 -0600

    Added Allocate Function

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include <iomanip>
 #include <fstream>
 #include <unistd.h>
-#include "A3.h"
+#include <vector>
 
 using namespace std;
 
@@ -14,18 +14,35 @@ using namespace std;
 void memoryManager(int, int);
 int allocate( int, int);
 
-
-
+struct page 
+{
+    int number;
+    int pid;
+    int frame;
+};
+struct process
+{
+    int pid;
+    int size;
+    page* pageTable;
+};
 
 // Global Variables
-void* freeFrameList;
+vector<int> freeFrameList;
+vector<process> processList;
+int* memory;
+
 
 int main()
 {
 
     cout << "Hello World\n";
-
-    memoryManager(30, 1);
+    process p;
+    p.pid =1;
+    p.size =2;
+    processList.insert(processList.end(), p);
+    memoryManager(10, 1);
+    cout << "Allocate: "<< allocate(2,1);
 
 
 
@@ -34,11 +51,16 @@ int main()
 
 void memoryManager(int memSize, int frameSize)
 {
-    int freeFrameList[memSize] = {0};
+
+    // we need allocated memory
+    // we need a list of free frames
+    memory = new int[memSize] {0};
+    
 
     for (int i =0; i < memSize; i++)
     {
-        cout << freeFrameList[i] << endl;
+        freeFrameList.push_back(i);
+        cout << memory[i] << endl;
     }
     
 }
@@ -47,7 +69,43 @@ void memoryManager(int memSize, int frameSize)
 int allocate(int allocSize, int pid)
 {
 
+    page* pages = new page[allocSize];
+    for (int i =0; i < allocSize; i++)
+    {
+        pages[i].number = 1;
+        pages[i].pid = pid;
+        
+    }
+
+    int processListIndex =0;
+    for (int i =0; i < processList.size(); i++)
+    {
+        if(processList[i].pid == pid)
+        {
+            processListIndex = i;
+            break;
+        }
+    }
+
+    // See if there are enough free frames
+    if (freeFrameList.size() < allocSize)
+    {
+        cout << "There are not enough free frames" << endl;
+        return -1;
+    }
+    else
+    {
+        processList[processListIndex].pageTable = new page[allocSize];
+        for(int i =0; i < allocSize; i++)
+        {
+            int randomFrame = rand() % freeFrameList.size();
+            processList[processListIndex].pageTable[i].frame = randomFrame;
+            freeFrameList.erase(freeFrameList.begin() + randomFrame);
+            memory[randomFrame] = pid;
+
+        }
+    }
 
 
-    return 0;
+    return 1;
 }
